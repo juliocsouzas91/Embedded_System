618d48d1234c33b3558c75a702fbbbb17869651f
Julio Souza <Juliocsouzas91@gmail.com>
2023-07-15 12:24:06 -0300

    Versao atualizada do aerador funcionando o motor no pin5 e somente um botao

--- Aeration_System_Seq/Aeration_System_Seq.ino
+++ Aeration_System_Seq/Aeration_System_Seq.ino
@@ -8,7 +8,7 @@ Autores: Julio Souza e Neudes...
 240 MHz (ESP32_DEFAULT_CPU_FREQ_240)
 */
 #define CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ ESP32_DEFAULT_CPU_FREQ_80
-#define motor_controler 32
+#define motor_controler 5
 #define time_seep 1000000
 #define button_on_pin 14
 #define button_off_pin 12
@@ -17,8 +17,6 @@ Autores: Julio Souza e Neudes...
 int *p_on, *p_off;
 int operation_status = 0;
 int minutos_on, minutos_off;
-uint64_t time_sleeping = 10000;
-
 
 
 void sleep(int *time_counter){
@@ -27,11 +25,11 @@ void sleep(int *time_counter){
 }
 
 void turn_on_motor(){
-  digitalWrite(motor_controler, 1);
+  digitalWrite(motor_controler, HIGH);
 }
 
 void turn_off_motor(){
-  digitalWrite(motor_controler, 0);
+  digitalWrite(motor_controler, LOW);
 }
 
 void IRAM_ATTR start_motor_button_int1(){
@@ -45,7 +43,6 @@ void IRAM_ATTR stop_motor_button_int2(){
 void setup() {
   Serial.begin(115200);
   pinMode(motor_controler, OUTPUT);
-  pinMode(button_on_pin, INPUT_PULLDOWN); //No pino a leitura sempre será 0 e o botão joga para 1 a ser pressionado 
 	pinMode(button_off_pin, INPUT_PULLUP); //No pino, a leitura sempre será 1 e o botão joga para 0 ao ser pressionado. 
   p_on = &minutos_on;
   p_off = &minutos_off;
@@ -59,38 +56,32 @@ void loop() {
   switch(operation_status){
     case 0:
       if(*p_off<1){
-          Serial.println(*p_off);
         sleep(&minutos_off);
       }
       else{
-          Serial.println("habilita interrupcao 1");
-        attachInterrupt(button_on_pin, start_motor_button_int1, RISING);
+        attachInterrupt(button_off_pin, start_motor_button_int1, FALLING);
         operation_status ++;
       }
     break;
 
     case 1:
-      if(*p_off<5){
-          Serial.println("caso 1");
+      if(*p_off<10){
         sleep(p_off);
       }
       else{
-        Serial.println("liga o motor");
         turn_on_motor();
+        detachInterrupt(button_off_pin);
         attachInterrupt(button_off_pin, stop_motor_button_int2, FALLING);
         operation_status ++;
       }
     break;
     case 2:
-      if(*p_on<5){
-          Serial.println("caso 2");
+      if(*p_on<10){
         sleep(p_on);
       }
       else{
-        Serial.println("desligado");
         *p_on = 0;
         *p_off = 0;
-        detachInterrupt(button_on_pin);
         detachInterrupt(button_off_pin);
         turn_off_motor();
         operation_status = 0;
