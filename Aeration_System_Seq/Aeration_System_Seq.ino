c2d50488962fed58fecf7121d05992975361d367
Julio Souza <Juliocsouzas91@gmail.com>
2023-07-28 17:47:12 -0300

    Adicionado Kit sensor de humidade

--- Aeration_System_Seq/Aeration_System_Seq.ino
+++ Aeration_System_Seq/Aeration_System_Seq.ino
@@ -9,13 +9,13 @@ Autores: Julio Souza e Neudes...
 */
 #define CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ ESP32_DEFAULT_CPU_FREQ_80
 #define motor_controler 5
-#define time_seep 1000000
+#define time_seep 60000000
 #define button_on_pin 14
 #define button_off_pin 12
-#define overflow_timer_value 72
+#define overflow_timer_value 72*60
 
 int *p_on, *p_off;
-int operation_status = 0;
+int operation_status;
 int minutos_on, minutos_off;
 
 
@@ -34,10 +34,12 @@ void turn_off_motor(){
 
 void IRAM_ATTR start_motor_button_int1(){
   *p_off = overflow_timer_value;
+  *p_on = 0;
 }
 
 void IRAM_ATTR stop_motor_button_int2(){
   *p_on = overflow_timer_value;
+  *p_off = 0;
 }
 
 void setup() {
@@ -49,34 +51,34 @@ void setup() {
   *p_on = 0;
   *p_off = 0;
   esp_sleep_enable_timer_wakeup(time_seep); // Define o tempo dormindo
-
+  operation_status = 0;
 }
 
 void loop() {
   switch(operation_status){
     case 0:
-      if(*p_off<1){
-        sleep(&minutos_off);
+      if(*p_off<5){
+        sleep(p_off);
       }
       else{
         attachInterrupt(button_off_pin, start_motor_button_int1, FALLING);
-        operation_status ++;
+        operation_status = 1;
       }
     break;
 
     case 1:
-      if(*p_off<10){
+      if(*p_off<30){
         sleep(p_off);
       }
       else{
         turn_on_motor();
         detachInterrupt(button_off_pin);
         attachInterrupt(button_off_pin, stop_motor_button_int2, FALLING);
-        operation_status ++;
+        operation_status = 2;
       }
     break;
     case 2:
-      if(*p_on<10){
+      if(*p_on<45){
         sleep(p_on);
       }
       else{
@@ -88,6 +90,6 @@ void loop() {
       }
     break;
     default:
-        turn_off_motor();
+      delay(200);
   }
 }
---
+++ Sensor_NTC/Sensor_NTC.ino
@@ -0,0 +1,10 @@
+
+
+void setup() {
+  Serial.begin(115200);// put your setup code here, to run once:
+}
+
+void loop() {
+
+
+}
